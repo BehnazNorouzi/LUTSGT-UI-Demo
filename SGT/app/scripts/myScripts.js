c0a7ad07ebd398c41305d23df0e9074c0993f184
Behnaz <BehnazNorouzi@users.noreply.github.com>
2016-10-31 15:13:27 +0200

    First commit

---
+++ SGT/app/scripts/myScripts.js
@@ -0,0 +1,25 @@
+//for add/Remove teeacher by Person In Charge
+//http://www.bootply.com/89084
+$('.collapse').on('shown.bs.collapse', function(){
+$(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+}).on('hidden.bs.collapse', function(){
+$(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+});
+
+
+
+
+$('.clickable').on('click', function (e) {
+  var $this = $(this);
+  if (!$this.hasClass('panel-collapsed')) {
+    $this.parents('.panel').find('.panel-body').slideUp();
+    $this.addClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-right');
+  } else {
+    $this.parents('.panel').find('.panel-body').slideDown();
+    $this.removeClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-down');
+  }
+})
+
+
